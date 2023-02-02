2338739e960c1008bd96b824560264abef09dd48
Jessica Duell <jessica.duell@pendo.io>
2023-02-02 15:27:36 -0500

    Added recursive searching

---
+++ recursive/recursive_searching.js
@@ -0,0 +1,25 @@
+/**
+ * Binary search
+ * 
+ * 1. Find the midpoint element
+ *     -round down if even elements
+ * 2. Compare the target agains the midpoint element
+ * 3. If the target matches the midpoint element return the index
+ * 4. Otherwise, determine which direction the binary search needs to go in
+ */
+
+const binary_search = (arr, target, start, end) => {}
+
+/**
+ * STRETCH
+ * 
+ * implement an order-agnostic binary search
+ * This version of binary search should correctly 
+ * find the target regardless of whether the input 
+ * array is sorted in ascending order or in 
+ * descending order. You can implement this function 
+ * either recursively or iteratively
+ */
+const agnostic_bianry_search = (arr, target) => {}
+
+module.exports = {binary_search, agnostic_bianry_search}
